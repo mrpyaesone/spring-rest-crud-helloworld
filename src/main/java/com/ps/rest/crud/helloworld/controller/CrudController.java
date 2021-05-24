eca9da9d3b039997c986bfa0bd8b05c064a6ccd9
Pyae Sone <myothantktt@gmail.com>
2021-05-25 02:13:57 +0630

    just creating fun for CRUD

--- src/main/java/com/ps/rest/crud/helloworld/controller/CrudController.java
+++ src/main/java/com/ps/rest/crud/helloworld/controller/CrudController.java
@@ -11,4 +11,24 @@ public class CrudController {
 	public String test() {
 		return "spring-rest-crud-helloworld Deployment is OK";
 	}
+	
+	@RequestMapping(value = "add", method = RequestMethod.POST)
+	public String insert() {
+		return "spring-rest-crud-helloworld Deployment is OK";
+	}
+	
+	@RequestMapping(value = "update", method = RequestMethod.POST)
+	public String update() {
+		return "spring-rest-crud-helloworld Deployment is OK";
+	}
+	
+	@RequestMapping(value = "remove", method = RequestMethod.POST)
+	public String delete() {
+		return "spring-rest-crud-helloworld Deployment is OK";
+	}
+	
+	@RequestMapping(value = "fetch", method = RequestMethod.GET)
+	public String select() {
+		return "spring-rest-crud-helloworld Deployment is OK";
+	}
 }
